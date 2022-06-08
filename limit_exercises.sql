820e6f1af9211c37a35e9474b1f24ebc5b185c3d
David Prieto <david.s.prieto1@gmail.com>
2022-06-08 14:23:37 -0500

    Completed MySQl - Clauses: limit exercises.

---
+++ limit_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT *
+FROM salaries;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT DISTINCT emp_no, salary
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
