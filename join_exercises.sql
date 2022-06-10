eba5bd307343fde9b9c7aa61c8477de98aaf8e84
David Prieto <david.s.prieto1@gmail.com>
2022-06-10 11:12:52 -0500

    Completed MySQL - Relationships: Joins exercises.

--- join_exercises.sql
+++ join_exercises.sql
@@ -60,3 +60,64 @@ JOIN roles ON users.role_id = roles.id
 GROUP BY role_name;
 
 # Part 2: Employees Database
+USE employees;
+
+#2
+SHOW tables;
+SELECT * FROM departments;
+SELECT * FROM dept_manager;
+
+SELECT departments.dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM employees
+    JOIN dept_manager
+        ON employees.emp_no = dept_manager.emp_no
+    JOIN departments
+        ON dept_manager.dept_no = departments.dept_no
+WHERE dept_manager.to_date = '9999-01-01'
+ORDER BY dept_name ASC;
+
+#3
+SELECT departments.dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM employees
+    JOIN dept_manager
+ON employees.emp_no = dept_manager.emp_no
+    JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+WHERE dept_manager.to_date = '9999-01-01' AND gender = 'F'
+ORDER BY dept_name ASC;
+
+#4
+SELECT title AS 'Title', COUNT(*) AS 'Count'
+FROM titles
+    JOIN dept_emp
+        ON titles.emp_no = dept_emp.emp_no
+    JOIN departments
+        ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Customer Service' AND dept_emp.to_date = '9999-01-01' AND titles.to_date = '9999-01-01'
+GROUP BY title;
+
+#5
+SELECT departments.dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager', salaries.salary AS 'Salary'
+FROM employees
+    JOIN dept_manager
+    ON employees.emp_no = dept_manager.emp_no
+    JOIN departments
+    ON dept_manager.dept_no = departments.dept_no
+    JOIN salaries
+        ON dept_manager.emp_no = salaries.emp_no
+WHERE dept_manager.to_date = '9999-01-01' AND salaries.to_date = '9999-01-01'
+ORDER BY dept_name ASC;
+
+#6
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(managers.first_name, ' ', managers.last_name) AS 'Manager'
+FROM employees
+    JOIN dept_emp AS de
+        ON de.emp_no = employees.emp_no
+    JOIN departments AS d
+        ON de.dept_no = d.dept_no
+    JOIN dept_manager AS m
+        ON m.dept_no = d.dept_no
+    JOIN employees AS managers
+        ON m.emp_no = managers.emp_no
+WHERE de.to_date > NOW() AND m.to_date > NOW()
+ORDER BY dept_name ASC;
